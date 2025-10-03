698f5258e85a515964e9d2174c2d7b26b851bc6b
JesSan224 <jes.12378924@gmail.com>
2025-10-02 19:16:30 -0500

    Subir PWA Tres en Raya

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "tres-en-raya-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./sw.js",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
